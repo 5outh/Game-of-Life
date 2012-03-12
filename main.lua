24085fbab6afce99b4e0b7d908119e80c2308838
Benjamin Kovach <bkovach13@gmail.com>
2012-03-12 14:44:39 -0400

    Init

--- main.lua
+++ main.lua
@@ -1,14 +1,26 @@
 function love.load()
-	--audioSource = love.audio.newSource("Spitfire.mp3", "stream")
-	--love.audio.play(audioSource)
-	xSize = love.graphics.getWidth()
-	ySize = love.graphics.getHeight()
-	xPosition, yPosition = (love.graphics.getWidth() + xSize)/2 , (love.graphics.getHeight() + ySize)/2
-	nodeSize = 10
+	ggg = love.filesystem.load("Presets/GosperGliderGun.lua")
+	crush = love.audio.newSource("crush.mp3", "stream")
+	love.graphics.setMode(1000, 800, false, true, 8)
+	largeFont = love.graphics.newFont(24)
+	smallFont = love.graphics.newFont(12)
 	
-	xNodes = xSize/nodeSize
-	yNodes = ySize/nodeSize
+	nodeSize = 12
+	xNodes = 75
+	yNodes = 50
+	xSize = xNodes * nodeSize
+	ySize = yNodes * nodeSize
 	
+	startButton = {
+		x = 20,
+		y = 8,
+		width = love.graphics.getWidth()/3 - 20,
+		height = love.graphics.getHeight()/15
+	}
+	
+	xPosition = (love.graphics.getWidth() - xSize- nodeSize) / 2
+	yPosition = (love.graphics.getHeight() - ySize- nodeSize) * 2/3
+
 	array = {0,0}
 	for i = 0, xNodes do
 		array[i] = {}
@@ -16,9 +28,12 @@ function love.load()
 			array[i][j] = 0
 		end
 	end
-	
 	gameStarted = false
-	love.graphics.setBackgroundColor(45, 3, 58)
+	rainbowMode = false
+	musicOn = false
+	
+	love.graphics.setBackgroundColor(34, 32, 40)
+	
 end
 
 function get_next(cur_array)
@@ -66,49 +81,108 @@ function love.update(dt)
 	if gameStarted then
 		array = get_next(array)
 	end
+	if love.mouse.isDown("r", "l") then
+		 x = love.mouse.getX() - xPosition
+		 y = love.mouse.getY() - yPosition
+		 if x > 0 and y > 0 then
+			xCoordinate = (x - (x % nodeSize)) /nodeSize 
+			yCoordinate = (y - (y % nodeSize)) /nodeSize
+			if xCoordinate < #array and yCoordinate < #array[1]then
+				if love.mouse.isDown("r") then
+					array[xCoordinate][yCoordinate] = 0
+				elseif love.mouse.isDown("l") then
+					array[xCoordinate][yCoordinate] = 1
+				end
+			end	
+		end
+	end
 end
 
 function love.draw(dt)
-	love.graphics.setColor(211, 231, 255)
+	
+	love.graphics.setColor(197, 199, 182) -- top banner and grid  (grey)
+	love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight()/12)
 	
 	step = nodeSize
 	while(step <= xSize) do
 		if(step <= ySize) then
-			love.graphics.line(nodeSize, step, xSize, step)
+			love.graphics.line(nodeSize+xPosition, step+yPosition, xSize+xPosition, step+yPosition)
 		end
-		love.graphics.line(step, nodeSize, step, ySize)
+		love.graphics.line(step+xPosition, nodeSize+yPosition, step+xPosition, ySize+yPosition)
 		step = step +nodeSize
 	end
-
+	
+	love.graphics.setColor(255,248,211) -- nodes (off-white)
 	for i = 1, xNodes-1 do
 		for j = 1, yNodes-1 do
 			if array[i][j] == 1 then
-				love.graphics.setColor(255,255,255)
-				love.graphics.rectangle('fill', i*nodeSize, j*nodeSize, nodeSize, nodeSize)
+				if rainbowMode then
+					love.graphics.setColor(math.random(255), math.random(255), math.random(255))
+					love.graphics.print("FUCKIN RAINBOWMODE", 360, 15, 0, 2)
+				end
+				love.graphics.rectangle('fill', i*nodeSize + xPosition, j*nodeSize + yPosition, nodeSize, nodeSize)
 			end
 		end
 	end
-end
-
-function click_update(x, y)
-	 xCoordinate = (x - (x % nodeSize)) /nodeSize 
-	 yCoordinate = (y - (y % nodeSize)) /nodeSize
-	 if xCoordinate < #array and yCoordinate < #array[1]then
-		array[xCoordinate][yCoordinate] = math.abs(array[xCoordinate][yCoordinate]-1)
-	 end
+	
+	love.graphics.setColor(47, 56, 55) -- top banner underline, start button
+	love.graphics.rectangle('fill', startButton.x, startButton.y, startButton.width, startButton.height)
+	love.graphics.rectangle('fill', 0, love.graphics.getHeight()/12,  love.graphics.getWidth(), love.graphics.getHeight()/192)
+	
+	if(not rainbowMode) then
+		love.graphics.setColor(255, 248, 211) -- off-white
+		love.graphics.setFont(largeFont)
+		if(gameStarted) then
+			love.graphics.print("Pause Game", startButton.x + 80, startButton.y + startButton.height /3, 0)
+		else
+			love.graphics.print("Start Game", startButton.x + 80, startButton.y + startButton.height /3, 0)
+		end
+	end
+	
+	--love.graphics.print(love.mouse.getX()..','..love.mouse.getY(), love.mouse.getX(), love.mouse.getY())
+	
 end
 
 function love.mousepressed(x, y, button)
    if button == "l" then
-	    click_update(x, y)
+		if(x >= startButton.x and x < startButton.x + startButton.width) then
+			if(y >= startButton.y and y <= startButton.y + startButton.height) then
+				if(rainbowMode) then
+					gameStarted = false
+					love.audio.pause(crush)
+					musicOn = false
+					rainbowMode = false
+				else
+					gameStarted = not gameStarted
+				end
+			end
+		end
    end
 end
 
 function love.keypressed(key)
-	if key == "1" then
-		gameStarted = true
+	if key == "r" then
+		rainbowMode = not rainbowMode
+		gameStarted = rainbowMode
+		musicOn = not musicOn
+		if(musicOn) then
+			love.audio.play(crush)
+			love.audio.resume(crush)
+		else
+			love.audio.pause(crush)
+		end
 	end
-	if key == "2" then
-		gameStarted = false
+	if key == "1" then
+		clearArray()
+	end
+end
+
+function clearArray()
+	array = {0,0}
+	for i = 0, xNodes do
+		array[i] = {}
+		for j = 0, yNodes do
+			array[i][j] = 0
+		end
 	end
 end
